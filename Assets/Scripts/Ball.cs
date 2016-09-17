45d0b442702be1b38418e675ae4f34a9f407f501
Nexemble <contact@nexemble.com>
2016-09-17 17:03:12 +0200

    1st commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rigid ;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigid = this.GetComponent<Rigidbody>();
+        rigid.AddForce(new Vector3(0.5f, 0.3f ,-0.8f)*5, ForceMode.Impulse);
+        Debug.Log("FORCE !!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TOUCH BALL !!");
+    }
+}
+   
